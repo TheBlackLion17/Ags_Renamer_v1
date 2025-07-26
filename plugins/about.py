4052a3bcd7297209fb6c64c7de8d4823cd12971f
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2025-07-26 11:51:31 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -2,10 +2,13 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from config import ABOUT_TEXT, SUPPORT_GROUP_URL, UPDATE_CHANNEL_URL
 from filter_plugins import force_sub
+from logger import logger # Import logger
 
 @Client.on_message(filters.command("about") & filters.private & force_sub)
 async def about_command(client: Client, message: Message):
     """Handles the /about command."""
+    user_id = message.from_user.id
+    logger.info(f"User {user_id} sent /about command.")
     keyboard = InlineKeyboardMarkup(
         [
             [
